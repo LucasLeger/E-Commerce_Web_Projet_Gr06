61949e6d2a761af1814e2236e5b078f09d10921e
Valentin <45029982+ValentinMorin@users.noreply.github.com>
2020-05-18 16:00:43 +0200

    Assign user role & panel

--- ecommerce/app/Http/Controllers/Admin/UsersController.php
+++ ecommerce/app/Http/Controllers/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Role;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -63,7 +64,12 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        $roles = Role::all();
+
+        return view('admin.users.edit', [
+            'user'=>$user,
+            'roles'=>$roles
+        ]);
     }
 
     /**
@@ -75,7 +81,8 @@ class UsersController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user->roles()->sync($request->roles);
+        return redirect()->route('admin.users.index');
     }
 
     /**
@@ -86,6 +93,9 @@ class UsersController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->roles()->detach();
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
     }
 }
