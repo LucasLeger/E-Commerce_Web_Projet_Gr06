3b02664e8c1383085809f87efe0e95d66be19386
Valentin <45029982+ValentinMorin@users.noreply.github.com>
2020-06-02 13:37:02 +0200

    Fix Parameters & User Lists

--- ecommerce/app/User.php
+++ ecommerce/app/User.php
@@ -52,9 +52,4 @@ class User extends Authenticatable
     {
         return $this->roles()->whereIn('name', $roles)->first();
     }
-
-    public function getRouteKeyName()
-    {
-        return $this->user()->where('name')->first();
-    }
 }
